9df66ce0c729c1fb1c0dd8dd5b97955a886b6e9c
piezzacondoleezza <76393917+piezzacondoleezza@users.noreply.github.com>
2022-12-14 23:32:40 +0100

    Update exec.py

--- task2/exec.py
+++ task2/exec.py
@@ -3,13 +3,13 @@ import subprocess
 import platform
 import time
 
-def check(host, MTU, os_type):
+def check(host, MTU, count, os_type):
     if os_type == 'darwin':
-        cmd = ['ping', '-D', '-s', str(MTU), str(host), '-c', '1', '-W', '3000']
+        cmd = ['ping', '-D', '-s', str(MTU), str(host), '-c', str(count), '-W', '3000']
     else:
-        cmd = ['ping', '-M', 'do', '-s', str(MTU), str(host), '-c', '1', '-W', '3']
+        cmd = ['ping', '-M', 'do', '-s', str(MTU), str(host), '-c', str(count), '-W', '3']
     res = subprocess.run(cmd, universal_newlines=True)
-    return res.returncode == 0
+    return res.returncode
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -17,20 +17,31 @@ parser.add_argument(
     required=True,
     help='host for which the minimal MTU is searched for'
 )
+parser.add_argument(
+    '--count',
+    required=False,
+    help='ping per time',
+    default=1,
+)
 
 args = parser.parse_args()
 host = args.host
 os_type = platform.system().lower()
+count = args.count
 
-left, right = 0, 1502 - 28
+left, right = 64 - 28, 1519 - 28
 while right - left > 1:
     mid = (left + right) // 2
-    if check(host, mid, os_type):
-        print('MTU {} is ok'.format(mid))
+    ret_code = check(host, mid, count, os_type)
+    if ret_code == 0:
+        print('MTU {} is ok'.format(28 + mid))
         left = mid
-    else:
-        print('MTU {} bad'.format(mid))
+    elif ret_code == 1:
+        print('MTU {} is bad'.format(28 + mid))
         right = mid
+    else:
+        print('failed')
+        exit(1)
     time.sleep(0.3)
 
-print('min MTU for {} is {}'.format(host, left))
+print('MTU for {} is {}'.format(host, left + 28))
